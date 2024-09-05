51232b3d550de241c630b22b9b7f444176d5fc42
nima70 <nima.shokofar@gmail.com>
2024-09-04 21:33:39 -0400

    init

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./src/**/*.tsx",
+        "./src/**/*.ts",
+        "./node_modules/keycloakify/**/*.js",
+        "./node_modules/keycloakify/**/*.jsx"
+    ],
+    theme: {
+        extend: {}
+    },
+    plugins: []
+};
